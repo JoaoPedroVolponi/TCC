27fd1d955379efd421ebed44b11f2b495d3318ad
João Pedro Volponi <jpvrosada@hotmail.com>
2023-11-01 00:21:43 -0300

    feat: 🎸 Criação dos elementos da View(HomeScreen)

--- Aplicacao/Aplicacao/Source/ViewController/HomeViewController.swift
+++ Aplicacao/Aplicacao/Source/ViewController/HomeViewController.swift
@@ -14,7 +14,6 @@ class HomeViewController: UIViewController {
 
     
     //MARK: - Life Cycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
--- Aplicacao/Aplicacao/View/HomeScreen.swift
+++ Aplicacao/Aplicacao/View/HomeScreen.swift
@@ -8,7 +8,64 @@
 import Foundation
 import UIKit
 
-
 class HomeScreen: UIView {
+
+    //MARK: - Elementos
     
+    lazy var body: UIView = {
+        let body = UIView()
+        body.translatesAutoresizingMaskIntoConstraints = false
+        body.backgroundColor = .white
+        body.addSubview(self.tableView)
+        return body
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.isScrollEnabled = true
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .white
+        tableView.register(ExchangeTableViewCell.self, forCellReuseIdentifier: ExchangeTableViewCell.identifier)
+        return tableView
+    }()
+
+    //MARK: - Construct
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(self.body)
+        self.configConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Methods
+    
+    public func delegateTableView(delegate: UITableViewDelegate, dataSource: UITableViewDataSource){
+        self.tableView.delegate = delegate
+        self.tableView.dataSource = dataSource
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+        
+            //TableView
+            self.tableView.topAnchor.constraint(equalTo: self.body.topAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: self.body.leadingAnchor, constant: 20),
+            self.tableView.trailingAnchor.constraint(equalTo: self.body.trailingAnchor, constant: -20),
+            self.tableView.bottomAnchor.constraint(equalTo: self.body.bottomAnchor),
+            
+            //Body
+            self.body.topAnchor.constraint(equalTo: self.topAnchor),
+            self.body.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            self.body.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            self.body.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+            
+        ])
+    }
 }
+
